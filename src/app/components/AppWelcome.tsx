f7453a6b9c065c54bb13c78387a63eb49fee4de3
Peepo <kanrayanapat@gmail.com>
2025-05-27 15:06:48 +0700

    add env

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,18 +1,32 @@
-export default function AppWelcome() {
-const title = 'Welcome to Peepo Cosci';
-const currentYear = <p>2025</p>;
-const istrue = true;
+'use client';
+
+import { Button } from "@/components/ui/button";
+
+type AppWelcomeProps = {
+    headTitle: string
+    isShow: boolean
+}
+
+export default function AppWelcome({headTitle, isShow}: AppWelcomeProps) {
+    const title = 'Welcome to Cosci';
+    const currentYear = <p>2025</p>
+    const handleClick = () => {
+        alert('Hello TypeScript');
+    }
 
     return (
         <>
-            <p>{title}</p>
+            <h1>{headTitle}</h1>
+            <p> {title.toUpperCase()}</p>
+            <Button onClick={handleClick}>Click Me</Button>
+            <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดได้เลย!</button>
             {currentYear}
             {
-                istrue && <p>Date: 10/10/1998</p>
+                isShow && <p>Date: 10/10/1998</p>
             }
-            {istrue ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
+            {
+                isShow ? <p> Hello Next.js </p> : <p> Hello Javascript </p>
             }
         </>
-
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,8 @@ import AppWelcome from "./components/AppWelcome";
 export default function Home() {
   return (
     <div>
-      <h1>
-        Home page
-      </h1>
-      <AppWelcome />
-      <AppWelcome />
-      <AppWelcome />
+      <h1>Home page</h1>
+      <AppWelcome headTitle="SWU" isShow={true} />
     </div>
   );
 }
