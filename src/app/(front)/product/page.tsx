fa467894dcaae32417bec8b0f89c45835efd4a4c
Peepo <kanrayanapat@gmail.com>
2025-05-29 13:04:08 +0700

    fetchdataProductfromMySQL

--- src/app/(front)/product/page.tsx
+++ src/app/(front)/product/page.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Select,
@@ -7,10 +8,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import db from "@/db";
+import { product } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import { ChevronRight } from "lucide-react";
 
-const Product = () => {
+const Product = async () => {
+  const products = await db.query.product.findMany({
+    orderBy: [desc(product.id)]
+});
+
   return (
-    <div className="max-w-screen-xl mx-auto pt-8 px-6 xl:px-0">
+    <div className="max-w-screen-xl mx-auto py-16 px-6 xl:px-0">
       <div className="flex items-end justify-between">
         <h2 className="text-3xl font-bold tracking-tight">Products</h2>
         <Select defaultValue="recommended">
@@ -19,36 +28,35 @@ const Product = () => {
           </SelectTrigger>
           <SelectContent>
             <SelectItem value="recommended">Recommended</SelectItem>
-            <SelectItem value="latest">Lastest</SelectItem>
+            <SelectItem value="latest">Latest</SelectItem>
             <SelectItem value="popular">Popular</SelectItem>
           </SelectContent>
         </Select>
       </div>
 
       <div className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
-        {[0, 1, 2, 3, 4, 5, 6, 7].map((i) => (
-          <Card key={i} className="shadow-none">
-            <CardHeader className="p-2">
-              <div className="aspect-video bg-muted rounded-lg w-full" />
+        {products.map((i) => (
+          <Card key={i.id} className="shadow-none overflow-hidden rounded-md">
+            <CardHeader className="p-0">
+              <div className="aspect-video bg-muted w-full border-b" />
             </CardHeader>
-            <CardContent className="pt-4 pb-5">
-              <Badge>Technology</Badge>
-
-              <h3 className="mt-4 text-[1.35rem] font-semibold tracking-tight">
-                What is the future of web development?
-              </h3>
-              <div className="mt-6 flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <div className="h-10 w-10 rounded-full bg-muted"></div>
-                  <span className="text-muted-foreground font-semibold">
-                    John Doe
-                  </span>
-                </div>
-
-                <span className="text-muted-foreground text-sm">
-                  Nov 30, 2024
+            <CardContent className="py-6">
+              <div className="flex items-center gap-3">
+                <Badge className="bg-primary/5 text-primary hover:bg-primary/5 shadow-none">
+                  Price: {i.price} 
+                </Badge>
+                <span className="font-medium text-xs text-muted-foreground">
+                  5 min read
                 </span>
               </div>
+
+              <h3 className="mt-4 text-[1.35rem] font-semibold tracking-tight">
+                {i.title}
+              </h3>
+
+              <Button className="mt-6 shadow-none">
+                Add to Cart <ChevronRight />
+              </Button>
             </CardContent>
           </Card>
         ))}
